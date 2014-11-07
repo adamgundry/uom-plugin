75cd33962d82bee6f9cc5ef9a713c53202592ccc
Adam Gundry <adam@well-typed.com>
2014-11-07 16:35:02 +0000

    Document Data.UnitsOfMeasure.TH

--- uom-plugin/src/Data/UnitsOfMeasure/TH.hs
+++ uom-plugin/src/Data/UnitsOfMeasure/TH.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 module Data.UnitsOfMeasure.TH
     ( u
     ) where
@@ -17,6 +18,21 @@ import Language.Haskell.TH.Quote
 
 import Data.UnitsOfMeasure
 
+-- | The 'u' quasiquoter may be used in a declaration, type or
+-- expression context, and its meaning depends on the context:
+--
+-- * in a declaration context, it creates new base and derived units
+--   from a comma-separated list of names with optional definitions,
+--   for example @[u|kg, m, s, N = kg * m/s^2|]@;
+--
+-- * in a type context, it parses a single unit and converts it into
+--   the corresponding type, so @[u|m/s|]@ becomes the type
+--   @Base "m" /: Base "s"@ of kind 'Unit';
+--
+-- * in an expression context, it can be used to assign units to a
+--   value, in conjunction with the '(%)' operator, e.g.
+--   @42 % [u|m|]@ is an expression of type @Quantity Integer [u|m|]@
+--
 u :: QuasiQuoter
 u = QuasiQuoter
       { quoteExp  = uExp
@@ -25,14 +41,18 @@ u = QuasiQuoter
       , quoteDec  = uDec
       }
 
+-- | Parse a unit expression and create a proxy that can be used with
+-- the '(%)' operator to create a quantity with those units.
 uExp :: String -> Q Exp
 uExp s = [| proxy# :: Proxy# $(uType s) |]
 
+-- | Parse a unit expression and convert it into the corresponding type.
 uType :: String -> Q Type
 uType s = case parseUnit universalSymbolTable s of
             Right expr -> reifyUnit expr
             Left  err  -> reportError ("unable to parse unit expression: " ++ err)  >> [t|()|]
 
+-- | Convert a unit expression into the corresponding type.
 reifyUnit :: UnitExp () String -> Q Type
 reifyUnit Unity        = [t| One |]
 reifyUnit (Unit _ s)   = [t| MkUnit $(litT (strTyLit s))            |]
@@ -48,7 +68,7 @@ uDec s = case parseUnitDecs s of
            Just xs -> concat <$> mapM (uncurry declareUnit) xs
            Nothing -> reportError ("unable to parse unit declarations: " ++ s) >> return []
 
--- | Parse a comma-separated list of unit declarations, for example
+-- | Parse a comma-separated list of unit declarations, for example:
 --
 -- > kg, m, s, N = kg * m/s^2
 parseUnitDecs :: String -> Maybe [(String, Maybe (UnitExp () String))]
@@ -65,8 +85,8 @@ parseUnitDecs = go
     go' u (',':xs) = ((u, Nothing) :) <$> go xs
     go' u ('=':xs) = let (d, ys) = break (== ',') xs
                      in case parseUnit universalSymbolTable d of
-                          Right exp -> ((u, Just exp) :) <$> go ys
-                          Left  _   -> Nothing
+                          Right e -> ((u, Just e) :) <$> go ys
+                          Left  _ -> Nothing
     go' _ _        = Nothing
 
 -- | Given a unit name and an optional definition, create an
