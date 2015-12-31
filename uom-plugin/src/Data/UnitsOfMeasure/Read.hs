a179fe360808475fca7e5f65042a88e0c2abbb9c
Adam Gundry <adam@well-typed.com>
2015-12-31 14:56:07 +0000

    Minor refactoring of readQuantity, and remove an unnecessary instance

--- uom-plugin/src/Data/UnitsOfMeasure/Read.hs
+++ uom-plugin/src/Data/UnitsOfMeasure/Read.hs
@@ -15,7 +15,7 @@ module Data.UnitsOfMeasure.Read
    , readUnit
    , readWithUnit
    , Some(..)
-   , SomeQuantity(..)
+   , QuantityWithUnit(..)
    ) where
 
 import Control.Monad (join)
@@ -28,14 +28,14 @@ import Text.Parse.Units (parseUnit, universalSymbolTable, UnitExp(..))
 import Data.UnitsOfMeasure.Internal
 import Data.UnitsOfMeasure.Singleton
 
-data SomeQuantity a where
-  SomeQuantity :: Quantity a (Pack u) -> SUnit u -> SomeQuantity a
+-- | Represents a quantity whose units have a syntactic representation
+-- that is known statically and at runtime.
+data QuantityWithUnit a u where
+  QuantityWithUnit :: Quantity a (Pack u) -> SUnit u -> QuantityWithUnit a u
 
+-- | An existential wrapper type: @'Some' p@ is essentially @exists x . p x@.
 data Some p where
   Some :: p x -> Some p
-
-instance Show (Some SUnit) where
-  show (Some u) = show (forgetSUnit u)
 
 instance (KnownUnit (Unpack u), u ~ Pack (Unpack u), Read a) => Read (Quantity a u) where
   readsPrec i (' ':s) = readsPrec i s
@@ -45,18 +45,19 @@ instance (KnownUnit (Unpack u), u ~ Pack (Unpack u), Read a) => Read (Quantity a
   readsPrec _ _ = []
 
 -- | Parse a quantity and check that it has the expected units.
-readWithUnit :: forall proxy a u . (Read a, KnownUnit u) => proxy u -> String -> Either String (Quantity a (Pack u))
+readWithUnit :: forall proxy a u . (Read a, KnownUnit u)
+             => proxy u -> String -> Either String (Quantity a (Pack u))
 readWithUnit _ s = do
-  SomeQuantity (q :: Quantity a _) v <- readQuantity s
+  Some (QuantityWithUnit (q :: Quantity a _) v) <- readQuantity s
   case testEquivalentSUnit (unitSing :: SUnit u) v of
     Just Refl -> Right q
     Nothing   -> Left ("wrong units: got " ++ show (forgetSUnit v))
 
 -- | Parse a quantity along with its units.
-readQuantity :: Read a => String -> Either String (SomeQuantity a)
+readQuantity :: Read a => String -> Either String (Some (QuantityWithUnit a))
 readQuantity s = case reads s of
                    [(n, s')] -> do Some u <- readUnit s'
-                                   return $ SomeQuantity (MkQuantity n) u
+                                   return $ Some (QuantityWithUnit (MkQuantity n) u)
                    _         -> Left "reads: no parse"
 
 -- | Parse a unit.
