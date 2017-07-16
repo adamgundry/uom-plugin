186b3d82bd8c8173ae370f765072acbc8b194ee4
Phil de Joux <philderbeast@gmail.com>
2017-07-16 16:08:42 -0400

    Add an hlint test suite.

---
+++ uom-plugin/hlint/HLint.hs
@@ -0,0 +1,16 @@
+module Main (main) where
+
+import Language.Haskell.HLint (hlint)
+import System.Exit (exitFailure, exitSuccess)
+
+arguments :: [String]
+arguments =
+    [ "src"
+    , "tests"
+    , "hlint"
+    ]
+
+main :: IO ()
+main = do
+    hints <- hlint arguments
+    if null hints then exitSuccess else exitFailure
