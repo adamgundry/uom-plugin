61fac9276e8fe2a6a580f5c85737427baad09b2f
Phil de Joux <philderbeast@gmail.com>
2017-07-16 16:46:45 -0400

    Use operator <$> over fmap.

--- uom-plugin/src/TcPluginExtras.hs
+++ uom-plugin/src/TcPluginExtras.hs
@@ -33,4 +33,4 @@ newWantedCt :: CtLoc -> PredType -> TcPluginM Ct
 newWantedCt loc = fmap mkNonCanonical . newWanted loc
 
 newGivenCt :: CtLoc -> PredType -> EvTerm -> TcPluginM Ct
-newGivenCt loc prd ev = fmap mkNonCanonical $ newGiven loc prd ev
+newGivenCt loc prd ev = mkNonCanonical <$> newGiven loc prd ev
