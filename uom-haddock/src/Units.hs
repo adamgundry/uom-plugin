e8f8a36e71f5b1dfbb8d3556d61d5957c19f483e
Phil de Joux <philderbeast@gmail.com>
2018-10-19 11:52:30 -0400

    Drop functions from module Units.

--- uom-haddock/src/Units.hs
+++ uom-haddock/src/Units.hs
@@ -1,16 +1,8 @@
 ﻿{-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Units
-    ( abs
-    , showRadian
-    , realToFrac'
-    ) where
+module Units where
 
-import Data.UnitsOfMeasure (u, convert, fromRational')
-import Data.UnitsOfMeasure.Internal (Quantity(..))
-import Data.Bifunctor.Flip (Flip(..))
-import Data.Ratio.Rounding (dpRound)
-
+import Data.UnitsOfMeasure (u)
 import Units.Angle ()
 
 [u| s, m |]
@@ -21,20 +13,3 @@ import Units.Angle ()
 [u| min = 60 s, h = 3600 s, d = 86400 s |]
 
 [u| ft = 100 % 328 m, mi = 1609.344 m, mph = mi/h |]
-
--- | Convert any 'Real' quantity into a 'Fractional' type ('realToFrac').
-realToFrac' :: (Real a, Fractional b) => Quantity a u -> Quantity b u
-realToFrac' (MkQuantity x) = MkQuantity (realToFrac x)
-
-instance Functor (Flip Quantity u) where
-    fmap = map'
-
-map' :: (a -> b) -> Flip Quantity u a -> Flip Quantity u b
-map' f (Flip (MkQuantity x)) = Flip $ MkQuantity $ f x
-
-showRadian :: Quantity Rational [u| rad |] -> String
-showRadian b = show dbl
-    where
-        deg = convert b :: Quantity Rational [u| deg |]
-        Flip rounded = dpRound 3 <$> Flip deg
-        MkQuantity dbl = fromRational' rounded :: Quantity Double [u| deg |]
